7f3b6e99c24dd069e729e7884b94ff4a9109b2bd
LysterDoCoding <acestar.lyster36@gmail.com>
2023-06-12 10:19:06 +0000

    JS Final Assesment by LysterDoCoding

--- playground.js
+++ playground.js
@@ -1,15 +1,50 @@
-/* 
-// Metacrafters - Beginning Javascript
-//
-// This is a javascript playground for testing your javascript code!
-// When you are ready to test, simply right click and select "Run Code"
-// to see the result print below. If you have more then one snippet of code,
-// you can highlight the code you want to test, and then right click and select "Run Code"
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
 */
 
-// Enter your code below this line
-
-// Code example
-for(var i=1; i<=5; i++) {
- console.log(i);
+// create a variable to hold your NFT's
+const NFTs = []
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_owner, _favcolor, _brandName, _item) {
+   const NFT = {
+      "owner": _owner,
+      "favcolor": _favcolor,
+      "brandName": _brandName,
+      "item": _item
+   }
+   NFTs.push(NFT);
+   console.log("Minted " + _owner);
 }
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+      for(let i = 0; i < NFTs.length; i++){
+         console.log ("\nNFT ID: \t\t\t" + (i + 1));
+         console.log("Owner Name: \t\t" + NFTs[i].owner);
+         console.log("Favorite Colour: \t" + NFTs[i].favcolor);
+         console.log("Brand Name: \t\t" + NFTs[i].brandName);
+         console.log("Item: \t\t\t\t" + NFTs[i].item);
+      }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+console.log("\nTotal Supplied: " + NFTs.length);
+}
+
+// call your functions below this line
+mintNFT("Lyster","Blue", "Aliodas", "Polo Shirt");
+mintNFT("KC","Yellow", "GoomBus", "Pants");
+mintNFT("Hazam","Violet", "League Of Leisures", "Watch");
+mintNFT("Dave","Red", "RRat", "Shoes");
+listNFTs();
+getTotalSupply()
